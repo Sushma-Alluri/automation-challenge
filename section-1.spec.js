5e552d06743cac9a3fca0c1a78dc716a03df4eda
Sushma-Alluri <91779140+Sushma-Alluri@users.noreply.github.com>
2021-10-04 15:05:21 +1100

    Add files via upload

--- section-1.spec.js
+++ section-1.spec.js
@@ -13,20 +13,21 @@ describe('Problem 1', () => {
     cy.visit('https://alayacare.com/')
 
   })*/
+  
   //visit localhost section-1
   it('Visits the localhost', () => {
     cy.visit('http://localhost:8080/section-1')
   })
 
   //testing for text containing DOM interactions
-  it('Visits the localhost', () => {
+  it('Tests the visibility- hidden of table', () => {
     cy.contains('DOM interactions');
   
   //Testing to check if the table is not visible
     cy.get('#alaya-table').should('be.hidden');
   })
 
-  it('Visits the localhost', () => {
+  it('Click the toggle button and check visibility', () => {
   //Clicking the button - Show table
     cy.get('.table-toggle-button').click();
 
@@ -35,17 +36,19 @@ describe('Problem 1', () => {
 
   })
 
-  it('Visits the localhost', () => {
+  it('Check number of colums', () => {
   //Testing to check if there are 5 table columns
-    cy.get('#alaya-table').find('.table-header').find('th').should('have.length', 5);
+    cy.get('#alaya-table').find('.table-header>th').should('have.length', 5);
+  })
 
+  it('Check number of rows excluding header', () => {
   //Testing to check if there are 10 rows excluding header
     cy.get('#alaya-table').find('tr:not(:first)').should('have.length', 10);
   })
 
-  //Testing to check if there are more than 5 
-  it('Visits the localhost', () => {
-    let count=0
+  //Testing to see that atleast 5 entries have the role user
+  it('Counts Role entries with value: user', () => {
+    let count=0;
     cy.get('#alaya-table').get("th:nth-child(5)")
       .each(($role, index, $Table) => {
         const text = $role.text();
@@ -54,22 +57,23 @@ describe('Problem 1', () => {
           }
       })
       .then(($Table) => {
-        expect(count).to.be.greaterThan(5);
+        expect(count).to.be.greaterThan(4);
       })
   })
 
-  //Testing to check if there are atleast 3 people over 60 years 
-  it('Visits the localhost', () => {
-    let count=0
+  //Testing to check if there are exactly 3 people over 60 years 
+  it('Counts number of people over 60years', () => {
+    let count=0;
     let now = new Date();
     cy.get('#alaya-table').get("th:nth-child(4)")
       .each(($age, index, $Table) => {
         var bDate = new Date($age.text()); //birth date
-        var age = now.getFullYear() - bDate.getFullYear();
+        var age = now.getFullYear() - bDate.getFullYear(); //calculate age by years
       if (age >= 60){
         count += 1;
       }
-    })
+      //if not 3 by year, check by months and days by using .getMonth() and .getDate() methods
+      })
     .then(($Table) => {
       expect(count).to.be.equal(3);
     })
